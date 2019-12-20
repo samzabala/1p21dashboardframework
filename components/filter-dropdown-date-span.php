5f50cd911f4d5149e88db7081b5176448e1ecd54
Sam Zabala <samapitongzabala@gmail.com>
2019-12-20 14:58:31 -0800

    oooo

--- components/filter-dropdown-date-span.php
+++ components/filter-dropdown-date-span.php
@@ -1,33 +1,36 @@
 
 <div class="module-function filter-dropdown-date-span-start">
 	<label class="input-label">From:</label>
-	
-	<button class="btn-group  btn-group-horizontal btn-group-unite" data-toggle="dropdown">
-		<span class="btn btn-default"><span class="current">Jan 2018</span></span>
-		<span class="btn btn-primary btn-symbol"><i class="symbol symbol-arrow-down"></i></span>
-	</button>
-	<!-- @REPLACE  #profile-assigned-date > li -->
-	<ul data-dropdown-width="167px" class="text-align-left dropdown dropdown-top-flush dropdown-right">
+	<span class="position-relative">
+		<button class="btn-group  btn-group-horizontal btn-group-unite" data-toggle="dropdown">
+			<span class="btn btn-default"><span class="current">Jan 2018</span></span>
+			<span class="btn btn-primary btn-symbol"><i class="symbol symbol-arrow-down"></i></span>
+		</button>
+		<!-- @REPLACE  #profile-assigned-date > li -->
+		<ul data-dropdown-width="100%" class="text-align-left dropdown dropdown-top-flush dropdown-right">
 
-		<li class="REPLACE">
-			<a href="#">Jan 2018</a>
-		</li>
-	</ul>
+			<li class="REPLACE">
+				<a href="#">Jan 2018</a>
+			</li>
+		</ul>
+	</span>
 </div>
 
 
 <div class="module-function filter-dropdown-date-span-end">
 	<label class="input-label">To:</label>
 	
-	<button class="btn-group  btn-group-horizontal btn-group-unite" data-toggle="dropdown">
-		<span class="btn btn-default"><span class="current">Jan 2018</span></span>
-		<span class="btn btn-primary btn-symbol"><i class="symbol symbol-arrow-down"></i></span>
-	</button>
-	<!-- @REPLACE  #profile-assigned-date > li -->
-	<ul data-dropdown-width="167px" class="text-align-left dropdown dropdown-top-flush dropdown-right">
+	<span class="position-relative">
+		<button class="btn-group  btn-group-horizontal btn-group-unite" data-toggle="dropdown">
+			<span class="btn btn-default"><span class="current">Jan 2018</span></span>
+			<span class="btn btn-primary btn-symbol"><i class="symbol symbol-arrow-down"></i></span>
+		</button>
+		<!-- @REPLACE  #profile-assigned-date > li -->
+		<ul data-dropdown-width="100%" class="text-align-left dropdown dropdown-top-flush dropdown-right">
 
-		<li class="REPLACE">
-			<a href="#">Jan 2018</a>
-		</li>
-	</ul>
+			<li class="REPLACE">
+				<a href="#">Jan 2018</a>
+			</li>
+		</ul>
+	</span>
 </div>
