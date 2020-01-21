a93574b3cf68b623538b254417275ce5abd27fcd
Sam Zabala <samapitongzabala@gmail.com>
2020-01-21 08:41:50 -0800

    dumb dropdown juice

--- assets/plugins/framework/js/framework.plugged.js
+++ assets/plugins/framework/js/framework.plugged.js
@@ -812,6 +812,8 @@ window.jQuery && jQuery.noConflict();
 	frameWork.toggleAccordion = function(triggerer) {
 		var selector =  _.getTheToggled(triggerer,'accordion');
 
+		console.log(selector);
+
 		if( selector ){
 			if(
 				!(
@@ -992,14 +994,14 @@ window.jQuery && jQuery.noConflict();
 				}else{
 
 					if(selector.closest('li , .nav-item').length > -1) {
-						selector.closest('li , .nav-item').siblings('li,.nav-item').find('.dropdown').slideUp(); 
+						// selector.closest('li , .nav-item').siblings('li,.nav-item').find('.dropdown').slideUp(); 
 						$(this).closest('li , .nav-item').siblings('li,.nav-item').find('*[data-toggle="dropdown"]').removeClass('open'); 
 						selector.closest('li , .nav-item').siblings('li,.nav-item').find('.dropdown').removeClass('open'); 
 					}
 
 
 					$('.dropdown').closest('li,.nav-item').removeClass('open'); 
-					$('.dropdown').slideUp();
+					// $('.dropdown').slideUp();
 					$('.dropdown').removeClass('open'); 
 					$('*[data-toggle="dropdown"]').removeClass('open'); 
 
@@ -1009,7 +1011,6 @@ window.jQuery && jQuery.noConflict();
 					selector.addClass('open'); 
 				}
 			}
-			
 		});
 
 
