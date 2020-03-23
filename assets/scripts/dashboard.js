6e462799ee82254e7b8dac9f09624adf3b4d1d7f
Sam Z <samapitongzabala@gmail.com>
2020-03-23 15:27:58 -0700

    oH MY GOD I AM DUUUUUMB

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -12,9 +12,6 @@
 // 		});
 // 	});
 //   }
-
-
-
 window.jQuery && jQuery.noConflict();
 (function($,fw,window){
 	//1p21 shit
@@ -53,33 +50,4 @@ window.jQuery && jQuery.noConflict();
 	window._1p21 = _1p21;
 
 
-	// $(function(){
-		
-
-		$('body').on('click', 'a', function(e) {
-			e.preventDefault();
-			console.log(e.target);
-		  });
-	// });
-
-	// fw.addEvent(document.body,'click',function(e){
-	// 	console.log('yeet');
-	// })
-	
-
 }(jQuery,frameWork,window));
-
-console.log('yaon si kuya');
-
-
-
-// jQuery('body').on({
-// 	click: function(e){
-// 		// console.log('yeet');
-// 		console.log(e.target)
-// 	},
-// 	mouseenter: function(e){ console.log('mouseenter',e.target) },
-// 	mouseleave: function(e){ console.log('mouseleave',e.target) },
-// }
-// // ,'a.js-client-opt'
-// );
