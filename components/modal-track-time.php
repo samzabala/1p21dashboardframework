c02e4ed63195ebf8c1ba87e8be132e6aae80d1ae
Sam Z <samapitongzabala@gmail.com>
2020-06-29 13:21:42 -0700

    lord

--- components/modal-track-time.php
+++ components/modal-track-time.php
@@ -1,16 +1,25 @@
 <div class="modal-track-time module-function">
 
 
+
+<!-- @NOTE:
+
+please update the btn-group only. actual is modal is on hold and will be updated later
+
+-->
+
 	<div class="btn-group btn-group-horizontal">
 
-		<!-- add entry -->
+		<!--
+			add entry 
+	-->
 		<a class="btn btn-primary-outline" href="#modal-track-time" data-toggle="modal">
 			Add Entry <i class="symbol symbol-plus"></i>
 		</a>
 
 		<!--
 			start timer
-			NOTE: display only when timer is paused
+			NOTE: render only when timer is paused
 		-->
 		<a class="btn btn-primary" href="#modal-track-time" data-toggle="modal">
 			Start Timer <i class="symbol symbol-play"></i>
@@ -19,7 +28,7 @@
 
 		<!--
 			pause timer
-			NOTE: display only when timer is active
+			NOTE: render only when timer is active
 		-->
 		<a class="btn btn-secondary" href="#modal-track-time" data-toggle="modal">
 			Pause Timer <i class="symbol symbol-pause"></i>
@@ -28,10 +37,7 @@
 	</div>
 
 
-<!-- @NOTE:
-ON STANDBY FOR DESIGNER'S UPDATES
 
--->
 	<div class="modal" id="modal-track-time" data-modal-max-width="960px" data-modal-title="Add new Entry">
 		<div class="container">
 
