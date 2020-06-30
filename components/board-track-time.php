5618334095755777860f2b90959c63f482f4d9f3
Sam Z <samapitongzabala@gmail.com>
2020-06-30 11:43:54 -0700

    js plugged + css + workflow assets + components

--- components/board-track-time.php
+++ components/board-track-time.php
@@ -26,7 +26,7 @@
 		</a>
 
 	</div>
-	<div class="board" id="board-track-time" data-board-max-width="960px" data-board-title="Add new Entry">
+	<div class="board" id="board-track-time" data-board-max-width="880px" data-board-title="Add new Entry">
 		<form action="" class="add-new-timer">
 			
 			<div class="p input-wrapper input-wrapper-vertical input-wrapper-block">
