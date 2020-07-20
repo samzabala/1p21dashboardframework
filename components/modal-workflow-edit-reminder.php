6560210c0b974c52d9d1e233d8a3de696e6a7089
Sam Z <samapitongzabala@gmail.com>
2020-07-20 13:10:28 -0700

    ee

--- components/modal-workflow-edit-reminder.php
+++ components/modal-workflow-edit-reminder.php
@@ -9,10 +9,21 @@
 		<textarea id="REPLACEMODE-REPLACEwithPostType-reminder-description-row[REPLACEWithRowIndex]" name="REPLACEMODE-REPLACEwithPostType-reminder-description-row[REPLACEWithRowIndex]" type="text" class="input input-block input-multiple-line" placeholder="Add reminder title/descriptior"></textarea>
 	</div>
 
-
-	<div class="input-wrapper input-wrapper-vertical input-wrapper-block p position-relative">
-		<label for="REPLACEMODE-REPLACEwithPostType-reminder-due-row[REPLACEWithRowIndex]" class="input-label">Due Date</label>
-		<input id="REPLACEMODE-REPLACEwithPostType-reminder-due-row[REPLACEWithRowIndex]" name="REPLACEMODE-REPLACEwithPostType-reminder-due-row[REPLACEWithRowIndex]" type="datetime" class="input input-block input-single-line" placeholder="mm/dd/yyyy" />
+	<div class="flex-grid flex-grid-no-gutter-y flex-grid-fixed">
+		<div class="flex-col-xs-12 flex-col-md-8">
+			
+			<div class="input-wrapper input-wrapper-vertical input-wrapper-block p position-relative">
+				<label for="REPLACEMODE-REPLACEwithPostType-reminder-due-date-row[REPLACEWithRowIndex]" class="input-label">Due Date</label>
+				<input id="REPLACEMODE-REPLACEwithPostType-reminder-due-date-row[REPLACEWithRowIndex]" name="REPLACEMODE-REPLACEwithPostType-reminder-due-date-row[REPLACEWithRowIndex]" type="date" class="input input-block input-single-line" placeholder="mm/dd/yyyy" />
+			</div>
+		</div>
+		<div class="flex-col-xs-12 flex-col-md-4">
+			
+			<div class="input-wrapper input-wrapper-vertical input-wrapper-block p position-relative">
+				<label for="REPLACEMODE-REPLACEwithPostType-reminder-due-time-row[REPLACEWithRowIndex]" class="input-label">Due Time</label>
+				<input id="REPLACEMODE-REPLACEwithPostType-reminder-due-time-row[REPLACEWithRowIndex]" name="REPLACEMODE-REPLACEwithPostType-reminder-due-time-row[REPLACEWithRowIndex]" type="time" class="input input-block input-single-line" placeholder="--:-- --" />
+			</div>
+		</div>
 	</div>
 
 	<div data-toggle="modal-close" class="btn btn-primary-outline btn-block"  onclick="REPLACEWithFnThatMakesChangesToCorrespondingInputFieldName">Set Reminder</div>
