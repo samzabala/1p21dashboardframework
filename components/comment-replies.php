8807a9989f50449360ace4f2c9895def3103166f
Sam Z <samapitongzabala@gmail.com>
2020-07-20 12:02:18 -0700

    task templates

--- components/comment-replies.php
+++ components/comment-replies.php
@@ -2,8 +2,7 @@
 	<div class="comment-replies p">
 		<a href="#" data-toggle="accordion">
 			<!-- @NOTE .REPLACE will have number of replies instead -->
-			
-			<i class="symbol symbol-arrow-down"></i> View <span class="REPLACE">69</span> Replies
+			<i class="symbol symbol-arrow-down"></i> View <span class="comment-reply-count"><span class="REPLACE">69</span></span> Replies
 		</a>
 		<div class="accordion dashboard-comment-replies-wrapper">
 
