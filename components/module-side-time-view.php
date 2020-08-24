eca27be6162d78531dbc0e0a5db6170f79cea292
Sam Z <samapitongzabala@gmail.com>
2020-08-24 13:24:52 -0700

    proj bois

--- components/module-side-time-view.php
+++ components/module-side-time-view.php
@@ -23,7 +23,8 @@ $args = app_parse_args($data,$defs);
 					</div>
 					<div class="flex-child">
 					
-						<?php if($args['post'] == 'task'): ?>
+						<!-- @TODO finalize apporpriate or remove uneeded conditional statement here -->
+						<?php if($args['post'] == 'task' || $args['post'] == 'project'): ?>
 							<a href="#"  data-toggle="modal" data-modal-title="REPLACwith<?=$args['post'];?>Title" class="btn btn-primary-outline btn-small">Time <i class="symbol symbol-plus"></i></a>
 							<?php app_get_component('components/modal-workflow-edit-task-time') ?>
 							&nbsp;
