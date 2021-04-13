090c7b1b3a14632ab5f727dbbadfcac9f542432b
samzabala <samapitongzabala@gmail.com>
2021-04-13 10:59:35 -0700

    fw/src/scss/* and app-outreach/components/modal-form-task

--- app-outreach/components/modal-board-form-task.php
+++ app-outreach/components/modal-board-form-task.php
@@ -6,8 +6,14 @@ Modal from framework/src/js/imports/modal
 -->
 <div class="modal modal-board" id="mb-editor-task" data-modal-title="Edit Task" data-modal-width="670px">
 	<form action="" class="flex-grid flex-grid-fixed">
+		<div class=" flex-col-xs-12">
+			<div class="input-wrapper input-wrapper-block input-wrapper-vertical">
+				<label for="task-title" class="input-label sr-only">Title</label>
+				<input type="text" name="task-title" class="input input-single-line input-large" placeholder="Add title">	
+			</div>
+		</div>
 
-		<div class="flex-col-md-8 flex-col-xs-12">
+		<div class="flex-col-md-5 flex-col-xs-12">
 			<div class="input-wrapper input-wrapper-block input-wrapper-vertical position-relative">
 				<label for="task-date" class="input-label">Scheduled Date</label>
 				<a data-toggle-dropdown class="btn-group btn-block btn-group-horizontal btn-group-unite">
@@ -18,26 +24,48 @@ Modal from framework/src/js/imports/modal
 						<i class="symbol symbol-calendar-alt"></i>
 					</div>
 				</a>
-				<div class="dropdown dropdown-top-flush dropdown-center-x no-padding">
-					<input type="text" name="task-date" class="input input-calendar no-radius-bottom" value="2069-04-20">
-					<a href="#" data-toggle-dropdown class="btn btn-primary btn-block no-radius-top">Set</a>
+				<div class="dropdown dropdown-top-flush dropdown-center-x no-padding" data-dropdown-width="100%">
+					<input type="text" name="task-date" class="input input-calendar" value="2069-04-20"/>
 				</div>
 			</div>
 		</div>
 
-		<div class="flex-col-md-4 flex-col-xs-12">
-			<div class="input-wrapper input-wrapper-block input-wrapper-vertical position-relative">
+		<div class="flex-col-md-3 flex-col-xs-12">
+			<div class="input-wrapper input-wrapper-block input-wrapper-vertical">
 				<label for="task-time" class="input-label">Scheduled Time</label>
 				<input type="time" name="task-time" class="input input-single-line">
 			</div>
 		</div>
 
+		<div class="flex-col-md-4 flex-col-xs-12">
+			<div class="input-wrapper input-wrapper-block input-wrapper-vertical">
+				<label for="task-asignee" class="input-label">Assignee</label>
+				<input type="text" name="task-asignee" class="input input-single-line" list="task-asignee-users">
+				<datalist id="task-asignee-users">
+					<option value="Phoenix Wright">
+					<option value="Miles Edgeworth">
+					<option value="Eren Yaeger">
+					<option value="Sasha Braus">
+					<option value="Harambe">
+					<option value="Inuyasha">
+					<option value="Batman">
+					<option value="Zack Snyder">
+				</datalist>
+			</div>
+		</div>
+
 		<div class=" flex-col-12">
 			<div class="input-wrapper input-wrapper-block input-wrapper-vertical position-relative">
-				<label for="task-time" class="input-label">Task Note</label>
+				<label for="task-note" class="input-label">Task Note</label>
 				<textarea class="input input-multiple-line" name="task-note">
 					REPLACE with tinymce
 				</textarea>
+			</div>
+		</div>
+		<div class=" flex-col-12">
+			<div class="input-wrapper input-wrapper-block input-wrapper-vertical position-relative">
+				<label for="task-time" class="input-label">Attachments</label>
+				<input type="file" multiple class="input input-single-line" name="task-attachments" />
 			</div>
 		</div>
 
