5ae5c2f22df4a09302113814e1f5c1965980f6ee
samzabala <samapitongzabala@gmail.com>
2021-04-13 11:21:47 -0700

    task modal sum more

--- app-outreach/components/modal-board-form-task.php
+++ app-outreach/components/modal-board-form-task.php
@@ -13,9 +13,9 @@ Modal from framework/src/js/imports/modal
 			</div>
 		</div>
 
-		<div class="flex-col-md-5 flex-col-xs-12">
+		<div class="flex-col-md-8 flex-col-xs-12">
 			<div class="input-wrapper input-wrapper-block input-wrapper-vertical position-relative">
-				<label for="task-date" class="input-label">Scheduled Date</label>
+				<label for="task-schedule-date" class="input-label">Scheduled Date</label>
 				<a data-toggle-dropdown class="btn-group btn-block btn-group-horizontal btn-group-unite">
 					<div class="btn btn-default justify-content-flex-start">
 						<span class="REPLACE">Friday, April 20, 2069</span>
@@ -25,32 +25,56 @@ Modal from framework/src/js/imports/modal
 					</div>
 				</a>
 				<div class="dropdown dropdown-top-flush dropdown-center-x no-padding" data-dropdown-width="100%">
-					<input type="text" name="task-date" class="input input-calendar" value="2069-04-20"/>
+					<input type="text" name="task-schedule-date" class="input input-calendar" value="2069-04-20"/>
+				</div>
+			</div>
+		</div>
+
+		<div class="flex-col-md-4 flex-col-xs-12">
+			<div class="input-wrapper input-wrapper-block input-wrapper-vertical">
+				<label for="task-schedule-time" class="input-label">Scheduled Time</label>
+				<input type="time" name="task-time" class="input input-single-line">
+			</div>
+		</div>
+
+		<div class="flex-col-md-5 flex-col-xs-12">
+			<div class="input-wrapper input-wrapper-block input-wrapper-vertical position-relative">
+				<label for="task-due-date" class="input-label">Due Date</label>
+				<a data-toggle-dropdown class="btn-group btn-block btn-group-horizontal btn-group-unite">
+					<div class="btn btn-default justify-content-flex-start">
+						<span class="REPLACE">Friday, April 20, 2069</span>
+					</div>
+					<div class="btn btn-symbol btn-default">
+						<i class="symbol symbol-calendar-alt"></i>
+					</div>
+				</a>
+				<div class="dropdown dropdown-top-flush dropdown-center-x no-padding" data-dropdown-width="100%">
+					<input type="text" name="task-due-date" class="input input-calendar" value="2069-04-20"/>
 				</div>
 			</div>
 		</div>
 
 		<div class="flex-col-md-3 flex-col-xs-12">
 			<div class="input-wrapper input-wrapper-block input-wrapper-vertical">
-				<label for="task-time" class="input-label">Scheduled Time</label>
-				<input type="time" name="task-time" class="input input-single-line">
+				<label for="task-due-time" class="input-label">Due Time</label>
+				<input type="time" name="task-due-time" class="input input-single-line">
 			</div>
 		</div>
 
 		<div class="flex-col-md-4 flex-col-xs-12">
 			<div class="input-wrapper input-wrapper-block input-wrapper-vertical">
 				<label for="task-asignee" class="input-label">Assignee</label>
-				<input type="text" name="task-asignee" class="input input-single-line" list="task-asignee-users">
-				<datalist id="task-asignee-users">
-					<option value="Phoenix Wright">
-					<option value="Miles Edgeworth">
-					<option value="Eren Yaeger">
-					<option value="Sasha Braus">
-					<option value="Harambe">
-					<option value="Inuyasha">
-					<option value="Batman">
-					<option value="Zack Snyder">
-				</datalist>
+				<select type="text" name="task-asignee" class="input input-single-line">
+					<option value="">Select User</option>
+					<option value="REPLACEId">Phoenix Wright</option>
+					<option value="REPLACEId">Miles Edgeworth</option>
+					<option value="REPLACEId">Eren Yaeger</option>
+					<option value="REPLACEId">Sasha Braus</option>
+					<option value="REPLACEId">Harambe</option>
+					<option value="REPLACEId">Inuyasha</option>
+					<option value="REPLACEId">Batman</option>
+					<option value="REPLACEId">Zack Snyder</option>
+				</select>
 			</div>
 		</div>
 
