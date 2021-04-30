49c2a3d105199bc350528be8f913201927d02c4a
samzabala <samapitongzabala@gmail.com>
2021-04-30 08:53:45 -0700

    update template analytics.php

--- app-workflow/analytics.php
+++ app-workflow/analytics.php
@@ -1,8 +1,11 @@
 <div id="workflow-analytics-team" class="module">
-	<div class="module-header">
+	<div class="module-header module-header-break">
 		<h3 class="module-title">
 			Avg Time by team members
 		</h3>
+		<div class="module-functions">
+			<?php app_get_component('components/filter-dropdown-date-span-maxmonth') ?>
+		</div>
 	</div>
 	<div class="module-content">
 		<?php app_get_component('components/chart-avg-team') ?>
