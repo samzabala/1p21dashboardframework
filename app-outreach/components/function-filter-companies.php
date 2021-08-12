f857ac74d16591431ccd22d7d19f429c4836af5e
samzabala <samapitongzabala@gmail.com>
2021-08-12 12:10:36 -0700

    company updates pa

--- app-outreach/components/function-filter-companies.php
+++ app-outreach/components/function-filter-companies.php
@@ -58,12 +58,56 @@
 </div>
 
 <div class="module-function function-filter-companies-acctowner">
-    <select class="input input-single-line" name="selecct-by-type" id="">
-        <option value="">Owner</option>
-        <!-- @placeholder replace/modify as needed -->
-        <option value="Hannah Habagat">Hannah Habagat</option>
-        <option value="Ammie Amihan">Ammie Amihan</option>
-        <option value="Bagyon Zuko">Bagyon Zuko</option>
-        <option value="Maliksi Kirstein">Maliksi Kirstein</option>
-    </select>
+<div class="position-relative">
+        <button data-toggle-dropdown class="input input-select">
+            Owner
+            <!-- @if one or more Owner is checked -->
+            <span class="filter-companies-acctowner-active">
+                <!-- @NOTE: acct owner name will be rendered here -->
+                (<span class="REPLACE">Yanni Yogi</span>)
+            </span>
+        </button>
+        <ul class="dropdown dropdown-top-flush dropdown-left" data-dropdown-width="200px">
+            <!-- @LOOP li -->
+                <li>
+                    <label class="input-label">
+                        <input type="checkbox" name="filter-companies-acctowner" value="profile_name" class="input input-inline">
+                        Profile Name
+                    </label>
+                </li>
+
+                <!-- @PLACEHOLDER delete when ready -->
+
+                    <li>
+                        <label class="input-label">
+                            <input type="checkbox" checked name="filter-companies-acctowner" value="Yanni Yogi" class="input input-inline">
+                            Yanni Yogi
+                        </label>
+                    </li>
+                    <li>
+                        <label class="input-label">
+                            <input type="checkbox" name="filter-companies-acctowner" value="El Tigre" class="input input-inline">
+                            El Tigre
+                        </label>
+                    </li>
+                    <li>
+                        <label class="input-label">
+                            <input type="checkbox" name="filter-companies-acctowner" value="Matt Engarde" class="input input-inline">
+                            Matt Engarde
+                        </label>
+                    </li>
+                    <li>
+                        <label class="input-label">
+                            <input type="checkbox" name="filter-companies-acctowner" value="Frank Sahwit" class="input input-inline">
+                            Frank Sahwit
+                        </label>
+                    </li>
+                    <li>
+                        <label class="input-label">
+                            <input type="checkbox" name="filter-companies-acctowner" value="Dee Vasquez" class="input input-inline">
+                            Dee Vasquez
+                        </label>
+                    </li>
+        </ul>
+    </div>
 </div>
