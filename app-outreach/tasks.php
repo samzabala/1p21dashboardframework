30aa4767e79468eb6f0f56172439455101f77e30
samzabala <samapitongzabala@gmail.com>
2021-10-05 14:27:26 -0700

    tasks template

--- app-outreach/tasks.php
+++ app-outreach/tasks.php
@@ -5,25 +5,29 @@
 	<div class="flex-col-xs-12 flex-col-sm-4 text-align-right">
 		<?php app_get_component('components/function-input-search') ?>
 		<div class="module-function">
-			<a  href="#mb-editor-task" data-modal-title="Add Task" data-toggle-modal-board class="btn btn-primary">
+			<a  href="#mb-editor-task" data-modal-title="Add Task" data-toggle-modal-board class="btn btn-primary btn-block-mobile">
 				Add New <i class="symbol symbol-plus"></i>
 			</a>
 		</div>
 	</div>
 </div>
 
-<div id="outreach-contacts" class="module module-no-gutter">
+<div id="outreach-tasks" class="module">
 
 
-	<div class="padding flex-xs flex-wrap justify-content-space-between">
-		<?app_get_component('components/pagination-view-per-page') ?>
-		<?app_get_component('components/pagination') ?>
+	<div class="module-header">
+		<div class="module-functions no-margin no-padding flex-xs flex-wrap justify-content-space-between">
+			<?php app_get_component('components/pagination-view-per-page') ?>
+
+			<?php app_get_component('components/pagination') ?>
+		</div>
 	</div>
-	<table class="table-fixed">
+	<table class="table-fixed outreach-table">
 		<tr>
-			<th class="text-align-center" width="50">
+			<th class="text-align-center outreach-table-mobile-th" width="50">
 				<form action="">
 					<input type="checkbox" class="input-inline" name="select-task-all" class="task-selector" id="select-task-all">
+					<label for="select-task-all" class="input-label hide-nonmobile">Select All Tasks</label>
 				</form>
 			</th>
 			<th class="text-nowrap">Description</th>
@@ -31,7 +35,7 @@
 			<th class="text-nowrap text-align-center" width="90">Due Date</th>
 			<th class="text-nowrap" width="130">Reminder</th>
 			<th class="text-nowrap text-align-center" width="100">Task Owner</th>
-			<th class="text-align-center" width="75">Status</th>
+			<th class="text-align-center" width="85">Status</th>
 			<th class="text-nowra text-align-right" width="50">
 				<div class="contact-actions position-relative">
 					<?php app_get_component('components/tooltip-actions','',false,array(
@@ -63,15 +67,18 @@
 		<tr>
 			<td class="text-align-center" width="50">
 				<form action="" class="task-c">
-					<input type="checkbox" class="input-inline" name="select-task-all" class="task-selector" id="select-task-REPLACEID">
+					<input type="checkbox" class="input-inline" name="select-task-REPLACEID" class="task-selector" id="select-task-REPLACEID">
+					<label for="select-task-REPLACEID" class="input-label color-neutral hide-nonmobile">Select Task</label>
 				</form>
 			</td>
 			<td width="350">
+				<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Description</span>
 				<div class="task-description">
 					<span class="REPLACE">Our school had a saying: “When something smells, it’s usually the Butz.” In the 23 years I’ve known him, it’s usually been true.</span>
 				</div>
 			</td>
 			<td width="200">
+				<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Contact</span>
 				<a class="color-inherit" href="<?= app_create_link(array('template'=>'contact-detail')) ?>">
 
 					<h5 class="task-contact-name no-margin-y">
@@ -83,11 +90,14 @@
 				</a>
 			</td>
 			<td class=" text-align-center" width="90">
+				<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Due Date</span>
 				<a href="#" class="task-due-date color-inherit text-nowrap">
 					<span class="RELPACE">4/20/69</span>
 				</a>
 			</td>
 			<td width="130">
+				<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Reminder</span>
+
 				<!-- @NOTE
 					.task-toggle-reminder-wrapper 
 						classes to add
@@ -95,7 +105,7 @@
 			
 				-->
 				<div class="task-toggle-reminder-wrapper flex-grid flex-grid-compact flex-grid-fixed justify-content-space-between flex-grid-no-gutter-y">
-					<div class="flex-col-5">
+					<div class="flex-col-xs-2 flex-col-sm-1 flex-col-md-5">
 						<div class="input-toggle input-large">
 							<input type="checkbox" class="input">
 							<label class="input-label input-toggle-label">
@@ -105,13 +115,13 @@
 							</label>
 						</div>
 					</div>
-					<div class="flex-col-6">
+					<div class="flex-col-xs-10 flex-col-sm-11 flex-col-md-6">
 						<!-- @if reminder is enabled -->
-						<div class="task-reminder-info">
-							<p class="font-size-small task-reminder-date no-margin color-primary font-weight-700">
+						<div class="task-reminder-info font-size-small ">
+							<p class="task-reminder-date no-margin color-primary font-weight-700">
 								<span class="REPLACE">4/20/69</span>
 							</p>
-							<p class="font-size-small task-reminder-time no-margin color-neutral">
+							<p class="task-reminder-time no-margin color-neutral">
 								<span class="REPLACE">4:20pm</span>
 							</p>
 						</div>
@@ -119,15 +129,17 @@
 				</div>
 			</td>
 			<td width="100" class="text-align-center" width="100">
+				<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Task Owner</span>
 				<a href="<?= app_create_link(array('template'=>'profile')) ?>" class="contact-acct-owner">
 					<?php app_get_component('components/profile-image-micro') ?>
 				</a>
 			</td>
-			<td class="text-align-center" width="75">
-				<div class="contact-type text-align-center">
+			<td class="text-align-center" width="85">
+				<div class="task-status">
+					<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Status</span>
 					<!-- @NOTE
 						.tag
-						classes to add => contact type
+						classes to add => task status tag
 							`tag-error` => Overdue 
 							`tag-caution` => Pending
 							`tag-success` => Completed
@@ -138,7 +150,7 @@
 					</span>
 				</div>
 			</td>
-			<td class="text-align-right">
+			<td class="outreach-table-cell-actions text-align-right">
 				
 				<div class="contact-actions position-relative">
 					<?php app_get_component('components/dropdown-actions','',false,array(
@@ -176,15 +188,18 @@
 					<tr>
 						<td class="text-align-center">
 							<form action="" class="task-c">
-								<input type="checkbox" class="input-inline" name="select-task-all" class="task-selector" id="select-task-REPLACEID">
+								<input type="checkbox" class="input-inline" name="select-task-REPLACEID" class="task-selector" id="select-task-<?=$i; ?>">
+								<label for="select-task-<?=$i; ?>" class="input-label color-neutral hide-nonmobile">Select Task</label>
 							</form>
 						</td>
 						<td width="350">
+							<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Description</span>
 							<div class="task-description">
 								<span class="REPLACE">Our school had a saying: “When something smells, it’s usually the Butz.” In the 23 years I’ve known him, it’s usually been true.</span>
 							</div>
 						</td>
-						<td width="200">
+							<td width="200">
+							<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Task</span>
 							<a class="color-inherit" href="<?= app_create_link(array('template'=>'contact-detail')) ?>">
 
 								<h5 class="task-contact-name no-margin-y">
@@ -196,11 +211,13 @@
 							</a>
 						</td>
 						<td class=" text-align-center">
+							<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Due Date</span>
 							<a href="#" class="task-due-date color-inherit text-nowrap">
 								<span class="RELPACE">4/20/69</span>
 							</a>
 						</td>
 						<td width="130">
+							<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Reminder</span>
 							<!-- @NOTE
 								.task-toggle-reminder-wrapper 
 									classes to add
@@ -208,7 +225,7 @@
 						
 							-->
 							<div class="task-toggle-reminder-wrapper flex-grid flex-grid-compact flex-grid-fixed justify-content-space-between flex-grid-no-gutter-y">
-								<div class="flex-col-5">
+								<div class="flex-col-xs-2 flex-col-sm-1 flex-col-md-5">
 									<div class="input-toggle input-large">
 										<input type="checkbox" class="input">
 										<label class="input-label input-toggle-label">
@@ -218,13 +235,13 @@
 										</label>
 									</div>
 								</div>
-								<div class="flex-col-6">
+								<div class="flex-col-xs-10 flex-col-sm-11 flex-col-md-6">
 									<!-- @if reminder is enabled -->
-									<div class="task-reminder-info">
-										<p class="font-size-small task-reminder-date no-margin color-primary font-weight-700">
+									<div class="task-reminder-info font-size-small">
+										<p class="task-reminder-date no-margin color-primary font-weight-700">
 											<span class="REPLACE">4/20/69</span>
 										</p>
-										<p class="font-size-small task-reminder-time no-margin color-neutral">
+										<p class="task-reminder-time no-margin color-neutral">
 											<span class="REPLACE">4:20pm</span>
 										</p>
 									</div>
@@ -232,15 +249,17 @@
 							</div>
 						</td>
 						<td width="100" class="text-align-center">
+							<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Task Owner</span>
 							<a href="<?= app_create_link(array('template'=>'profile')) ?>" class="contact-acct-owner">
 								<?php app_get_component('components/profile-image-micro') ?>
 							</a>
 						</td>
 						<td class="text-align-center">
-							<div class="contact-type text-align-center">
+							<div class="task-status">
+								<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Status</span>
 								<!-- @NOTE
 									.tag
-									classes to add => contact type
+									classes to add => task status tag
 										`tag-error` => Overdue 
 										`tag-caution` => Pending
 										`tag-success` => Completed
@@ -251,7 +270,7 @@
 								</span>
 							</div>
 						</td>
-						<td class="text-align-right" width="50">
+						<td class="outreach-table-cell-actions text-align-right" width="50">
 							
 							<div class="contact-actions position-relative">
 								<?php app_get_component('components/dropdown-actions','',false,array(
@@ -286,8 +305,10 @@
 				<?php } ?>
 	</table>
 
-	<div class="padding flex-xs flex-wrap justify-content-space-between">
-		<?app_get_component('components/pagination-view-per-page') ?>
-		<?app_get_component('components/pagination') ?>
+	<div class="module-footer">
+		<div class="module-functions no-margin no-padding flex-xs flex-wrap justify-content-space-between">
+			<?php app_get_component('components/pagination-view-per-page') ?>
+			<?php app_get_component('components/pagination') ?>
+		</div>
 	</div>
 </div>
