d72f5598f21b46c52a26cbf5c075c9715716ab2c
samzabala <samapitongzabala@gmail.com>
2021-12-03 08:36:36 -0800

    disregard sync fw

--- app-outreach/components/module-tags.php
+++ app-outreach/components/module-tags.php
@@ -14,22 +14,22 @@
     </div>
     <div class="module-content">
         <!-- @if has tags -->
-            <span class="tag tag-neutral">
+            <span class="tag outreach-palette-tag-company">
                 <span class="REPLACE">Tag content here</span>
             </span>
 
             <!-- @PLACEHOLDER: DELETE WHEN READY -->
-                <span class="tag tag-neutral">Talent Agency</span>
-                <span class="tag tag-neutral">Law Firm</span>
-                <span class="tag tag-neutral">Japanifornia Locations</span>
-                <span class="tag tag-neutral">Herlock Sholmes</span>
-                <span class="tag tag-neutral">Metal Detector</span>
-                <span class="tag tag-neutral">Spirit Medium Services</span>
-                <span class="tag tag-neutral">Jollibee</span>
-                <span class="tag tag-neutral">Eat your hamburgers Apollo</span>
-                <span class="tag tag-neutral">Can defeat Thanos</span>
-                <span class="tag tag-neutral">Defense Attorney for Orcas in Los Angeles California</span>
-                <span class="tag tag-neutral">Incoming Edge Case Tag That has a lot of words in it We're no strangers to love You know the rules and so do I A full commitment's what I'm thinking of You wouldn't get this from any other guy  I just wanna tell you how I'm feeling Gotta make you understand  Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you  We've known each other for so long Your heart's been aching but you're too shy to say it Inside we both know what's been going on We know the game and we're gonna play it And if you ask me how I'm feeling Don't tell me you're too blind to see  Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you  Ooh (Give you up) Ooh-ooh (Give you up) Ooh-ooh Never gonna give never gonna give (Give you up) Ooh-ooh Never gonna give never gonna give (Give you up) We've known each other for so long Your heart's been aching but you're too shy to say it Inside we both know what's been going on We know the game and we're gonna play it  I just wanna tell you how I'm feeling Gotta make you understand  Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you </span>
+                <span class="tag outreach-palette-tag-company">Talent Agency</span>
+                <span class="tag outreach-palette-tag-company">Law Firm</span>
+                <span class="tag outreach-palette-tag-company">Japanifornia Locations</span>
+                <span class="tag outreach-palette-tag-company">Herlock Sholmes</span>
+                <span class="tag outreach-palette-tag-company">Metal Detector</span>
+                <span class="tag outreach-palette-tag-company">Spirit Medium Services</span>
+                <span class="tag outreach-palette-tag-company">Jollibee</span>
+                <span class="tag outreach-palette-tag-company">Eat your hamburgers Apollo</span>
+                <span class="tag outreach-palette-tag-company">Can defeat Thanos</span>
+                <span class="tag outreach-palette-tag-company">Defense Attorney for Orcas in Los Angeles California</span>
+                <span class="tag outreach-palette-tag-company">Incoming Edge Case Tag That has a lot of words in it We're no strangers to love You know the rules and so do I A full commitment's what I'm thinking of You wouldn't get this from any other guy  I just wanna tell you how I'm feeling Gotta make you understand  Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you  We've known each other for so long Your heart's been aching but you're too shy to say it Inside we both know what's been going on We know the game and we're gonna play it And if you ask me how I'm feeling Don't tell me you're too blind to see  Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you  Ooh (Give you up) Ooh-ooh (Give you up) Ooh-ooh Never gonna give never gonna give (Give you up) Ooh-ooh Never gonna give never gonna give (Give you up) We've known each other for so long Your heart's been aching but you're too shy to say it Inside we both know what's been going on We know the game and we're gonna play it  I just wanna tell you how I'm feeling Gotta make you understand  Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you Never gonna give you up Never gonna let you down Never gonna run around and desert you Never gonna make you cry Never gonna say goodbye Never gonna tell a lie and hurt you </span>
 
         <!-- @else -->
             <p class="color-neutral font-style-italic">
