3796e5e79267995599b5c6bc2032b5a388d7c0a6
samzabala <samapitongzabala@gmail.com>
2022-01-26 15:48:11 -0800

    template updates

---
+++ app-outreach/components/modal-form-bulk.php
@@ -0,0 +1,9 @@
+<!--
+Imports JS:
+
+Modal from framework/src/js/imports/modal
+
+-->
+<div id="m-editor-bulk" class="modal modal-default"  data-modal-width="500px">
+	<?php app_get_component('components/bulk-form') ?>
+</div>
