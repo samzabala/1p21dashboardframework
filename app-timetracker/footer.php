fd1a489414817c45c4791433471f14b433980408
samzabala <samapitongzabala@gmail.com>
2022-08-16 10:13:19 -0700

    footer

---
+++ app-timetracker/footer.php
@@ -0,0 +1,13 @@
+
+
+<!-- boards that appear -->
+<?php if(FWAPPS_TEMPLATE !== 'debug'): ?>
+	
+<?php endif; ?>
+					
+					<?php app_debug_block(); ?>
+				</div>
+			</main>
+		</div>
+	</body>
+</html>
