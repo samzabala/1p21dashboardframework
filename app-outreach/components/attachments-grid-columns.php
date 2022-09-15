1923676cd3701520becaeb2227094ea435e184ba
samzabala <samapitongzabala@gmail.com>
2022-09-15 13:02:49 -0700

    attachments component

---
+++ app-outreach/components/attachments-grid-columns.php
@@ -0,0 +1,31 @@
+<div class="flex-grid flex-grid-fixed">
+
+<!-- @loop .attachment-item -->
+	<div class="attachment-item flex-col-xs-6 flex-col-sm-4">
+		<?php app_get_component('components/attachment-item-columns','',false,array(
+			'file_extension' => 'REPLACEWithFileExtension'
+		)); ?>
+	</div>
+
+	<!-- @PLACEHOLDER: delete when ready -->
+		<?php
+		$file_ext_demo = array(
+			'csv',
+			'doc',
+			'pdf',
+			'txt',
+			'jpg',
+		);
+		for($i=0; $i<count($file_ext_demo); $i++){ ?>
+
+			<div class="attachment-item flex-col-xs-6 flex-col-sm-4">
+				<?php app_get_component('components/attachment-item-columns','',false,array(
+					'file_extension' => $file_ext_demo[$i]
+				)); ?>
+			</div>
+		<?php } ?>
+
+
+
+
+</div>
