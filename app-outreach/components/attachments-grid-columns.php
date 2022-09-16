8706cb1b62f1f12e328d081ff3c531e28e812551
samzabala <samapitongzabala@gmail.com>
2022-09-16 14:41:21 -0700

    attachments component update

--- app-outreach/components/attachments-grid-columns.php
+++ app-outreach/components/attachments-grid-columns.php
@@ -1,7 +1,7 @@
 <div class="flex-grid flex-grid-fixed">
 
 <!-- @loop .attachment-item -->
-	<div class="attachment-item flex-col-xs-6 flex-col-sm-4">
+	<div class="attachment-item flex-col-xs-6 flex-col-sm-4 flex-xs align-items-stretch">
 		<?php app_get_component('components/attachment-item-columns','',false,array(
 			'file_extension' => 'REPLACEWithFileExtension'
 		)); ?>
@@ -18,7 +18,7 @@
 		);
 		for($i=0; $i<count($file_ext_demo); $i++){ ?>
 
-			<div class="attachment-item flex-col-xs-6 flex-col-sm-4">
+			<div class="attachment-item flex-col-xs-6 flex-col-sm-4 flex-xs align-items-stretch">
 				<?php app_get_component('components/attachment-item-columns','',false,array(
 					'file_extension' => $file_ext_demo[$i]
 				)); ?>
