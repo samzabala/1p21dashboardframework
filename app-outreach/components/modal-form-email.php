ffd20753e23f5d40103c045b1a3446602d8430d2
samzabala <samapitongzabala@gmail.com>
2022-09-30 09:55:15 -0700

    modal form markup

---
+++ app-outreach/components/modal-form-email.php
@@ -0,0 +1,10 @@
+<!--
+Imports JS:
+
+Modal from framework/src/js/imports/modal
+
+-->
+<div class="modal modal-constricted modal-default" id="m-editor-email" data-modal-close="false" data-modal-width="960px" data-modal-fullscreen-content-display="flex">
+
+	<?php app_get_component('components/message-form'); ?>
+</div>
