e07ec2e335a9436e7fa27a9e7c8a46474d787c92
samzabala <samapitongzabala@gmail.com>
2022-09-30 10:22:36 -0700

    shtuff

--- app-outreach/footer.php
+++ app-outreach/footer.php
@@ -3,10 +3,10 @@
 <!-- boards that appear -->
 <?php if(FWAPPS_TEMPLATE !== 'debug'): ?>
 <?php app_get_component('components/modal-form-swimlane'); ?>
+<?php app_get_component('components/modal-form-email') ?>
 <?php app_get_component('components/modal-board-form-contact') ?>
 <?php app_get_component('components/modal-board-form-task') ?>
 <?php app_get_component('components/modal-board-form-event') ?>
-<?php app_get_component('components/modal-board-form-email') ?>
 <?php app_get_component('components/modal-board-form-company') ?>
 <?php app_get_component('components/modal-board-detail-task') ?>
 <?php app_get_component('components/modal-board-detail-event') ?>
