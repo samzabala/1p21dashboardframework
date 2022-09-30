7d6c412e64f5c2a3c84f3aad2d1d4f05d2ddfa90
samzabala <samapitongzabala@gmail.com>
2022-09-30 09:55:15 -0700

    modal form markup

--- app-outreach/components/modal-board-form-email.php
+++
@@ -1,9 +0,0 @@
-<!--
-Imports JS:
-
-Modal from framework/src/js/imports/modal
-
--->
-<div class="modal modal-board" id="mb-editor-email" data-modal-title="Compose Email" data-modal-width="770px">
-	In progress pa
-</div>
