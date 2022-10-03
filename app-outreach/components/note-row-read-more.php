67a8d7b4aa84afda3bd6c03d340b3b3e4858f94c
samzabala <samapitongzabala@gmail.com>
2022-10-03 11:47:29 -0700

    note row subcomponents

---
+++ app-outreach/components/note-row-read-more.php
@@ -0,0 +1,5 @@
+
+<a href="#" data-toggle-modal-default>Read More</a>
+<div class="modal modal-default" data-modal-title="REPLACE With Email Subject" data-modal-width="800px" data-modal-close="true">
+	<?php app_get_component('components/_DEMO-html-markup'); ?>
+</div>
