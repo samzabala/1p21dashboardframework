3af88ab21e89d9e3b1e5d1115757551ae16f419b
samzabala <samapitongzabala@gmail.com>
2023-02-06 10:57:37 -0800

    login template

---
+++ app-timetracker-ext/login.php
@@ -0,0 +1,23 @@
+<div id="timetracker-login">
+	<main id="timetracker-main" class=" flex-xs flex-wrap justify-content-center align-items-center timetracker-body-login">
+		<div class="module">
+			<div class="module-header">
+				<div class="h1 text-align-center margin-top">Log In</div>
+			</div>
+			<div class="module-content">
+				<div class="flex-grid flex-grid-fixed">
+					<div class="flex-col-xs-12">
+						<button href="#" class="btn btn-block btn-neutral-outline no-padding-x">
+							<img class="margin-small-right" src="<?=FWAPPS_ROOT_URL ?>/app-<?=FWAPPS_APP?>/assets/images/icon-wrike.svg" alt=""> Login with Wrike
+						</button>
+					</div>
+					<div class="flex-col-xs-12">
+						<button href="#" class="btn btn-block btn-neutral-outline no-padding-x">
+							<img class="margin-right" src="<?=FWAPPS_ROOT_URL ?>/app-<?=FWAPPS_APP?>/assets/images/icon-microsoft.svg" alt=""> Login with Office365
+						</button>
+					</div>
+				</div>
+			</div>
+		</div>
+	</main>
+</div>
