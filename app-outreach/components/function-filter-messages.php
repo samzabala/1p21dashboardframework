5364f075651e34748f68ba85cccaf1859c0068ad
samzabala <samapitongzabala@gmail.com>
2023-02-24 14:15:56 -0800

    replicate app filter. can disregard

---
+++ app-outreach/components/function-filter-messages.php
@@ -0,0 +1,31 @@
+
+
+
+<?php app_get_component('components/module-function-dropdown','',false,array(
+	'title' => 'User',
+    'slug' => 'acctowner',
+    'count' => 1,
+	'items' => array(
+        'All Users',
+        'Franky Franklin',
+        'Fiona Frost',
+        'Donovan Desmond',
+        'Miles Medgeworth',
+        'Becky Blackbell',
+    ),
+)) ?>
+
+
+<?php app_get_component('components/module-function-dropdown','',false,array(
+	'title' => 'Owner',
+    'slug' => 'acctowner',
+    'count' => 1,
+	'items' => array(
+        'All Users',
+        'Yanni Yogi',
+        'El Tigre',
+        'Matt Engarde',
+        'Frank Sahwit',
+        'Dee Vasquez',
+    ),
+)) ?>
