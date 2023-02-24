259a49a831ec8cd30fb4b6786be2ed62832f9076
samzabala <samapitongzabala@gmail.com>
2023-02-24 15:52:06 -0800

    replicate app filter. can disregard

--- app-outreach/components/function-filter-messages.php
+++ app-outreach/components/function-filter-messages.php
@@ -1,31 +1,37 @@
 
+<div class="module-function function-filter-acctowner position-relative">
+
+    <div class="input-wrapper input-wrapper-horizontal input-wrapper-block-mobile input-wrapper-responsive no-padding-right">
+        <label class="input-label">Filter By</label>
+	</div>
+</div>
 
 
-<?php app_get_component('components/module-function-dropdown','',false,array(
-	'title' => 'User',
-    'slug' => 'acctowner',
-    'count' => 1,
-	'items' => array(
-        'All Users',
-        'Franky Franklin',
-        'Fiona Frost',
-        'Donovan Desmond',
-        'Miles Medgeworth',
-        'Becky Blackbell',
-    ),
-)) ?>
+	<?php app_get_component('components/module-function-dropdown','',false,array(
+		'title' => 'User',
+		'slug' => 'acctowner',
+		'count' => 1,
+		'items' => array(
+			'All Users',
+			'Franky Franklin',
+			'Fiona Frost',
+			'Donovan Desmond',
+			'Miles Medgeworth',
+			'Becky Blackbell',
+		),
+	)) ?>
 
 
-<?php app_get_component('components/module-function-dropdown','',false,array(
-	'title' => 'Owner',
-    'slug' => 'acctowner',
-    'count' => 1,
-	'items' => array(
-        'All Users',
-        'Yanni Yogi',
-        'El Tigre',
-        'Matt Engarde',
-        'Frank Sahwit',
-        'Dee Vasquez',
-    ),
-)) ?>
+	<?php app_get_component('components/module-function-dropdown','',false,array(
+		'title' => 'Owner',
+		'slug' => 'acctowner',
+		'count' => 1,
+		'items' => array(
+			'All Users',
+			'Yanni Yogi',
+			'El Tigre',
+			'Matt Engarde',
+			'Frank Sahwit',
+			'Dee Vasquez',
+		),
+	)) ?>
