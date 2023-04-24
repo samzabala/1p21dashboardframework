5e9d712b89075539be4f076b016c44a5d354f239
samzabala <samapitongzabala@gmail.com>
2023-04-24 09:53:58 -0700

    project details

---
+++ app-timetracker/project-detail-tasks.php
@@ -0,0 +1,8 @@
+<!-- Breadcrumb -->
+<?php app_get_component('components/manage-breadcrumbs') ?>
+
+<!-- Tabs -->
+<?php app_get_component('components/project-nav-tabs') ?>
+
+<!-- Tasks -->
+<?php app_get_component('components/module-project-tasks') ?>
