0bc2d03ca58f88b404acc94a6df5abdcd759aac6
samzabala <samapitongzabala@gmail.com>
2023-04-24 09:51:13 -0700

    client details

---
+++ app-timetracker/client-detail.php
@@ -0,0 +1,12 @@
+<!-- Breadcrumb -->
+<?php app_get_component('components/manage-breadcrumbs') ?>
+
+<!-- client Info -->
+<?php app_get_component('components/module-client-editor') ?>
+
+<!-- projects -->
+<?php app_get_component('components/module-client-projects') ?>
+
+
+<!-- @PLACEHOLDER: Demo only: delete when ready -->
+	<?php app_get_component('components/_DEMO-detail-editor-tags-subheader') ?>
