c176086b974b97a993416b9f7def4aee72782c1b
samzabala <samapitongzabala@gmail.com>
2023-04-24 09:55:03 -0700

    task details

---
+++ app-timetracker/task-detail-time.php
@@ -0,0 +1,8 @@
+<!-- Breadcrumb -->
+<?php app_get_component('components/manage-breadcrumbs') ?>
+
+<!-- Tabs -->
+<?php app_get_component('components/task-nav-tabs') ?>
+
+<!-- Tasks -->
+<?php app_get_component('components/module-task-time') ?>
