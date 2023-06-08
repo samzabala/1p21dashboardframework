a0685c583f76a970169dbe5d41461fe8da78bdca
samzabala <samapitongzabala@gmail.com>
2023-06-08 10:34:10 -0700

    sidebar

--- app-timetracker/includes/nav-sidebar.php
+++ app-timetracker/includes/nav-sidebar.php
@@ -1,11 +1,13 @@
 
-<a class="timetracker-logo nav-block justify-content-center padding-small-x  flex-0-0" href="<?=app_create_link(); ?>">
+<a id="timetracker-logo " class="nav-block justify-content-center padding-small-x  flex-0-0" href="<?=app_create_link(); ?>">
 	<?php app_get_component('components/icon-logo'); ?>
 </a>
-<button data-toggle-accordion class="only-mobile timetracker-nav-toggle btn background-theme-polar-active color-theme-polar-active btn-symbol btn-no-radius border-color-transparent position-relative flex-0-0">
+<button data-toggle-accordion class="only-mobile timetracker-nav-toggle btn background-theme-polar-active color-theme-polar-active btn-symbol btn-no-radius border-color-transparent position-relative flex-0-0 position-fixed offset-0-top offset-0-right">
 	<i class="symbol symbol-burger symbol-close-toggle"></i>
 </button>
-<ul class="nav-list timetracker-nav-list nav-list-responsive accordion accordion-mobile">
+<div class="accordion accordion-mobile timetracker-nav-accordion flex-1-1 flex-xs flex-direction-column">
+
+<ul class="nav-list  timetracker-nav-list nav-list-responsive flex-1-1 margin-large-bottom">
 	<!-- @NOTE : templates these link to are not finalized yet. we may comment these out for now while their not sent yet -->
 
 	<!-- Track Pages -->
@@ -110,6 +112,57 @@
 			</a>
 		</li>
 </ul>
-<div class="modal" id="edit-email">
-	Wait Lang
+
+<div id="timetracker-sidebar-utility" class="margin-auto-top padding-x padding-small-y position-sticky offset-0-bottom offset-0-x">
+			
+	<div class=" flex-grid flex-grid-compact flex-grid-no-gutter-y align-items-center flex-nowrap">
+
+		<!-- You -->
+		<div class="flex-child flex-0-0">
+
+			<div class="thumbnail thumbnail-small radius-small">
+				<!-- @IF has profile imahe -->
+					<div class="thumbnail-image radius-small">
+						<img class="profile-image" src="<?=FWAPPS_ROOT_URL ?>/placeholder/profiles/team-des-jenn.jpg" alt="">
+					</div>
+				
+				<!-- @ELSE -->
+					<span class="thumbnail-text profile-name-initial">
+						<span class="REPLACE">PN</span>
+					</span>
+
+			</div>
+		</div>
+		<div class="flex-child flex-1-1">
+			<span class="text-wrap-ellipsis">
+				<span class="REPLACE">Profile</span>
+			</span>
+		</div>
+		<div class="flex-child flex-0-0">
+			<a data-toggle-dropdown class="font-size-large color-inherit color-neutral-light-hover btn-symbol padding-y">
+				<i class="symbol symbol-kebab-horizontal"></i>
+			</a>
+			<ul class="dropdown position-absolute  dropdown-bottom-flush dropdown-right padding" data-dropdown-width="100%" data-dropdown-max-height="300px">
+				<li>
+					<a href="<?=app_create_link(array('template'=> 'profile')); ?>" class="color-inherit-hover position-relative">
+						Profile
+					</a>
+				</li>
+				<li>
+					<a href="<?=app_create_link(array('template'=> 'settings')); ?>" class="color-inherit-hover position-relative">
+						Settings
+					</a>
+				</li>
+				<li>
+					<a href="#" class="color-inherit-hover position-relative">
+						Logout
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+</div>
+
+
 </div>
