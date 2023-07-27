185926dbb968de5dcc4c04a54c9ac422e0fab5b7
samzabala <samapitongzabala@gmail.com>
2023-07-27 10:14:16 -0700

    clients list

--- app-timetracker/components/list-clients.php
+++ app-timetracker/components/list-clients.php
@@ -28,10 +28,60 @@
 			'jolliwasp.com (PPC)',
 		),
 	);
+	$placeholder_ppl = array(
+		array('JA',FWAPPS_ROOT_URL.'/placeholder/profiles/team-des-jenn.jpg'),
+		array('NR',FWAPPS_ROOT_URL.'/placeholder/profiles/team-dev-nadia.jpg'),
+		array('GC',FWAPPS_ROOT_URL.'/placeholder/profiles/team-dev-garrett.jpg'),
+		array('JE',FWAPPS_ROOT_URL.'/placeholder/profiles/team-des-jenna.jpg'),
+		array('SZ',FWAPPS_ROOT_URL.'/placeholder/files/shrek.jpg'),
+	);
 	?>
  <div class="table-wrapper">
-	<table class="timetracker-table border-style-solid-bottom border-width-thin-bottom border-color-neutral-alpha-3">
+	<table class="timetracker-table border-style-solid-bottom border-width-thin-bottom border-color-neutral-alpha-3 text-vertical-align-middle">
 	
+		<tr class="hide-mobile">
+			<th class="font-weight-700 color-theme text-align-center color-theme timetracker-td timetracker-td-w timetracker-td-w-50">
+				<label class="input-label no-padding">
+					<input type="checkbox" class="input-inline">
+					<span class="hide-nonmobile">Select all clients</span>
+				</label>
+			</th>
+			<th class="font-weight-700 color-theme">
+				Client
+				<!-- a @NOTES: classes to add:
+					'active' => when ascending order
+					'' => when descending order
+				-->
+				<a href="#" class="color-inherit"><i class="symbol symbol-caret-down symbol-caret-up-toggle"></i></a>
+			</th>
+			<th class="font-weight-700 color-theme text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+				Sales
+			</th>
+			<th class="font-weight-700 color-theme text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+				Account Manager
+			</th>
+			<th class="font-weight-700 color-theme text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+				Project Manager
+			</th>
+			<th class="font-weight-700 color-theme text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+				SEO
+			</th>
+			<th class="font-weight-700 color-theme">Projects</th>
+			<th class="no-border-bottom timetracker-td-actions text-align-center" width="50">
+				<div class="bulk-actions position-relative">
+					<?php app_get_component('components/modal-actions','',false,array(
+						'links' => array(
+							'Bulk delete'
+								=> '#',
+							'Bulk update'
+								=> '#',
+							'Bulk something'
+								=> '#',
+						)
+					)) ?>
+				</div>
+			</th>
+		</tr>
 		<!--
 		@if this task has entries
 		@loop template / tr and tbody
@@ -39,12 +89,11 @@
 		-->
 		<!-- <template> -->
 			<tr>
-				<td class="hide-mobile no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-75 padding-left">
-					
-					<!-- @if has projects -->
-						<a href="#entry-projects-REPLACEID" class="btn btn-small btn-primary-outline btn-symbol btn-round " data-toggle-accordion>
-							<i class="symbol symbol-arrow-down symbol-arrow-up-toggle"></i>
-						</a>
+				<td class="text-align-center no-border-bottom timetracker-td text-align-center timetracker-td timetracker-td-w timetracker-td-w-50">
+					<label class="input-label no-padding">
+						<input type="checkbox" class="input-inline">
+						<span class="hide-nonmobile">Select client</span>
+					</label>
 				</td>
 				<td class="no-border-bottom timetracker-td">
 					<a href="<?= app_create_link(array('template' => 'client-detail')); ?>" class="h4 color-inherit font-weight-400 no-margin-y display-inline-block">
@@ -53,29 +102,92 @@
 						</span>	
 					</a>
 				</td>
-				<td class="no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
-					<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Date Created</span>
-					<i class="symbol symbol-file"></i>
-					<span class="REPLACE">April 20</span>
+				<td class="text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+					<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Sales</span>
+					<?php app_get_component('components/thumbnail-small'); ?>
+				</td>
+				<td class="text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+					<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Account Manager</span>
+					<?php app_get_component('components/thumbnail-small'); ?>
+				</td>
+				<td class="hide-mobile text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+					<span class="spacer">&nbsp;</span>
+				</td>
+				<td class="hide-mobile text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+					<span class="spacer">&nbsp;</span>
+				</td>
+				<td class="no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-75 text-align-center">
+					
+					<!-- @if has projects -->
+						<a class="hide-nonmobile" href="#entry-projects-REPLACEID"  data-toggle-accordion>
+							<i class="symbol symbol-arrow-down symbol-arrow-up-toggle"></i> View projects
+						</a>
+						<a href="#entry-projects-REPLACEID" class="hide-mobile btn btn-small btn-primary-outline btn-symbol btn-round " data-toggle-accordion>
+							<i class="symbol symbol-arrow-down symbol-arrow-up-toggle"></i>
+						</a>
+				</td>
+				<td class="no-border-bottom timetracker-td-actions text-align-center" width="50">
+					<div class="bulk-actions position-relative">
+						<?php app_get_component('components/modal-actions','',false,array(
+							'links' => array(
+								'Edit'
+									=> '#',
+								'Delete'
+									=> '#',
+							)
+						)) ?>
+					</div>
 				</td>
 			</tr>
 				<tbody class="accordion" id="entry-projects-REPLACEID">
 					<!--@loop .tr  -->
-						<tr>
-							<td class="hide-mobile padding-left">
+						<tr class="background-primary-alpha-1">
+							<td class="hide-mobile no-border-bottom timetracker-td text-align-center timetracker-td timetracker-td-w timetracker-td-w-50">
 								<span class="spacer">&nbsp;</span>
 							</td>
 							<td class="timetracker-td">
-								<a href="<?= app_create_link(array('template' => 'project-detail')); ?>" class="no-margin-y display-inline-block color-inherit">
-									<span class="REPLACE">
-										ace-attorney.com
-									</span>	
-								</a>
+								<span class="flex-md">
+									<label class="input-label no-padding margin-right">
+										<input type="checkbox" class="input-inline">
+										<span class="hide-nonmobile">Select client</span>
+									</label>
+									<br class="hide-nonmobile">
+									<br class="hide-nonmobile">
+									<a href="<?= app_create_link(array('template' => 'project-detail')); ?>" class="no-margin-y display-inline-block color-inherit">
+										<span class="REPLACE">
+											ace-attorney.com
+										</span>	
+									</a>
+								</span>
 							</td>
-							<td class="no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
-								<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Date Created</span>
-								<i class="symbol symbol-file"></i>
-								<span class="REPLACE">April 20</span>
+							<td class="hide-mobile text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+								<span class="spacer">&nbsp;</span>
+							</td>
+							<td class="hide-mobile text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+								<span class="spacer">&nbsp;</span>
+							</td>
+							<td class="text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+								<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Project Manager</span>
+								<?php app_get_component('components/thumbnail-small'); ?>
+							</td>
+							<td class="text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+								<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">SEO</span>
+								<?php app_get_component('components/thumbnail-small'); ?>
+							</td>
+							<td class="no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-75 text-align-center">
+								<span class="spacer">&nbsp;</span>
+							</td>
+							<td class="no-border-bottom timetracker-td-actions text-align-center" width="50">
+								<div class="bulk-actions position-relative">
+									<?php app_get_component('components/modal-actions','',false,array(
+										'links' => array(
+											'Edit'
+												=> '#',
+											'Delete'
+												=> '#',
+										)
+									)) ?>
+								</div>
 							</td>
 						</tr>
 				</tbody>
@@ -84,11 +196,11 @@
 
 			<?php $h = 0; foreach($placeholder_cli as $cli => $projects): ?>
 				<tr>
-					<td class="hide-mobile no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-75 padding-left">
-						
-							<a href="#entry-projects-<?=$h ?>" class="btn btn-small btn-primary-outline btn-symbol btn-round " data-toggle-accordion>
-								<i class="symbol symbol-arrow-down symbol-arrow-up-toggle"></i>
-							</a>
+					<td class="text-align-center no-border-bottom timetracker-td text-align-center timetracker-td timetracker-td-w timetracker-td-w-50">
+						<label class="input-label no-padding">
+							<input type="checkbox" class="input-inline">
+							<span class="hide-nonmobile">Select client</span>
+						</label>
 					</td>
 					<td class="no-border-bottom timetracker-td">
 						<a href="<?= app_create_link(array('template' => 'client-detail')); ?>" class="h4 color-inherit font-weight-400 no-margin-y display-inline-block">
@@ -97,30 +209,104 @@
 							</span>	
 						</a>
 					</td>
-					<td class="no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
-						<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Date Created</span>
-						<i class="symbol symbol-file"></i>
-						<span class="REPLACE">April 20</span>
+					<td class="text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+						<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Project Manager</span>
+						<?php app_get_component('components/thumbnail-small','',false,array(
+							'initials' => $placeholder_ppl[$h][0],
+							'image' => $placeholder_ppl[$h][1]
+						)); ?>
+					</td>
+					<td class="text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+						<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Account Manager</span>
+						<?php app_get_component('components/thumbnail-small','',false,array(
+							'initials' => $placeholder_ppl[$h][0],
+							'image' => $placeholder_ppl[$h][1]
+						)); ?>
+					</td>
+					<td class="hide-mobile text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+						<span class="spacer">&nbsp;</span>
+					</td>
+					<td class="hide-mobile text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+						<span class="spacer">&nbsp;</span>
+					</td>
+					<td class="no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-75 text-align-center">
+						
+							<a class="hide-nonmobile" href="#entry-projects-<?=$h ?>" data-toggle-accordion>
+								<i class="symbol symbol-arrow-down symbol-arrow-up-toggle"></i> View projects
+							</a>
+							<a href="#entry-projects-<?=$h ?>" class="hide-mobile btn btn-small btn-primary-outline btn-symbol btn-round " data-toggle-accordion>
+								<i class="symbol symbol-arrow-down symbol-arrow-up-toggle"></i>
+							</a>
+					</td>
+					<td class="no-border-bottom timetracker-td-actions text-align-center" width="50">
+						<div class="bulk-actions position-relative">
+							<?php app_get_component('components/modal-actions','',false,array(
+								'links' => array(
+									'Edit'
+										=> '#',
+									'Delete'
+										=> '#',
+								)
+							)) ?>
+						</div>
 					</td>
 				</tr>
 					<tbody class="accordion" id="entry-projects-<?=$h ?>">
 					<?php $i = 0; foreach($projects as $proj): ?>
 							<!--@loop .tr  -->
-								<tr>
+								<tr class="background-primary-alpha-1">
 									<td class="hide-mobile padding-left">
 										<span class="spacer">&nbsp;</span>
 									</td>
 									<td class="timetracker-td">
-										<a href="<?= app_create_link(array('template' => 'project-detail')); ?>" class="no-margin-y display-inline-block color-inherit">
-											<span class="REPLACE">
-												<?= $proj ?>
-											</span>	
-										</a>
+										<span class="flex-md">
+											<label class="input-label no-padding margin-right">
+												<input type="checkbox" class="input-inline">
+												<span class="hide-nonmobile">Select client</span>
+											</label>
+											<br class="hide-nonmobile">
+											<br class="hide-nonmobile">
+											<a href="<?= app_create_link(array('template' => 'project-detail')); ?>" class="no-margin-y display-inline-block color-inherit">
+												<span class="REPLACE">
+													<?= $proj ?>
+												</span>	
+											</a>
+										</span>
 									</td>
-									<td class="no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
-										<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Date Created</span>
-										<i class="symbol symbol-file"></i>
-										<span class="REPLACE">April 20</span>
+									<td class="hide-mobile text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+										<span class="spacer">&nbsp;</span>
+									</td>
+									<td class="hide-mobile text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+										<span class="spacer">&nbsp;</span>
+									</td>
+									<td class="text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+										<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">Project Manager</span>
+										<?php app_get_component('components/thumbnail-small','',false,array(
+											'initials' => $placeholder_ppl[$h][0],
+											'image' => $placeholder_ppl[$h][1]
+										)); ?>
+									</td>
+									<td class="text-align-center no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-150">
+										<span class="hide-nonmobile no-margin outreach-table-mobile-label h6 color-neutral">SEO</span>
+										<?php app_get_component('components/thumbnail-small','',false,array(
+											'initials' => $placeholder_ppl[$h][0],
+											'image' => $placeholder_ppl[$h][1]
+										)); ?>
+									</td>
+									<td class="no-border-bottom timetracker-td timetracker-td-w timetracker-td-w-75 text-align-center">
+										<span class="spacer">&nbsp;</span>
+									</td>
+									<td class="no-border-bottom timetracker-td-actions text-align-center" width="50">
+										<div class="bulk-actions position-relative">
+											<?php app_get_component('components/modal-actions','',false,array(
+												'links' => array(
+													'Edit'
+														=> '#',
+													'Delete'
+														=> '#',
+												)
+											)) ?>
+										</div>
 									</td>
 								</tr>
 						<?php endforeach; ?>
