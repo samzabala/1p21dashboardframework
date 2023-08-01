c279aa73ca186c6a2c968bc45852e8479becb973
samzabala <samapitongzabala@gmail.com>
2023-08-01 08:51:55 -0700

    icon components

---
+++ app-timetracker/components/icon-order-ascending.php
@@ -0,0 +1,9 @@
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 width="6.886px" height="16px" viewBox="0 0 6.886 16" enable-background="new 0 0 6.886 16" xml:space="preserve">
+<g>
+	<path fill="currentColor" opacity="0.25" d="M4.337,15.447l2.442-4.883c0.332-0.665-0.151-1.447-0.894-1.447H1.001c-0.743,0-1.227,0.782-0.894,1.447
+		l2.442,4.883C2.917,16.184,3.969,16.184,4.337,15.447z"/>
+	<path fill="currentColor" d="M2.548,0.553L0.107,5.436c-0.332,0.665,0.151,1.447,0.894,1.447h4.883c0.743,0,1.227-0.782,0.894-1.447L4.337,0.553
+		C3.969-0.184,2.917-0.184,2.548,0.553z"/>
+</g>
+</svg>
---
+++ app-timetracker/components/toggle-order.php
@@ -0,0 +1,9 @@
+
+<a href="#" class="color-inherit text-decoration-none text-vertical-align-middle display-inline-flex">
+	<!-- @if is descending -->
+		<?php app_get_component('components/icon-order-descending'); ?>
+	<!-- @if is ascending -->
+		<?php app_get_component('components/icon-order-ascending'); ?>
+	<!-- @else -->
+		<?php app_get_component('components/icon-order-none'); ?>
+</a>
