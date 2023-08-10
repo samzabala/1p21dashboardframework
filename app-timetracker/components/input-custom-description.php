99bc744d18b28095808613cf9b755fce7e0b2563
samzabala <samapitongzabala@gmail.com>
2023-08-10 11:08:01 -0700

    component - input description

---
+++ app-timetracker/components/input-custom-description.php
@@ -0,0 +1,11 @@
+<div class="margin-small-top color-neutral-dark">
+
+	<div class="flex-xs pointer-reference">
+		<div class="flex-0-0 margin-small-right">
+			<i class="symbol symbol-pencil"></i>
+		</div>
+		<span class="font-style-italic flex-1-1">
+			<input type="text" class="input input-blend input-block"  placeholder="Click here to a description +" value="REPLACE A description for this field">
+		</span>
+	</div>
+</div>
