93c50253e48ac01b23fb2e44372cc5d722a97a76
samzabala <samapitongzabala@gmail.com>
2023-08-25 11:06:18 -0700

    header updates

--- app-timetracker-ext/header.php
+++ app-timetracker-ext/header.php
@@ -14,9 +14,9 @@
 			
 		<!-- SCRIPT -->
 			
-			<!-- DEPENDENCIES -->
-				<script src="<?=FWAPPS_ROOT_URL.'/app-'.FWAPPS_APP.'/assets/scripts/framework.customglobal.mine.min.js'?>" ></script>
-				<!--<script src="<?=FWAPPS_ROOT_URL.'/assets/plugins/framework/dist/js/framework.lib.js'?>" ></script>-->
+			<!-- @DEMO -->
+				<!-- <script src="<?=FWAPPS_ROOT_URL.'/app-'.FWAPPS_APP.'/assets/scripts/framework.customglobal.mine.min.js'?>" ></script> -->
+				<script src="<?=FWAPPS_ROOT_URL.'/assets/plugins/framework/dist/js/framework.lib.js'?>" ></script>
 
 		<!-- @PLACEHOLDER for debugging framework. delete when ready. or repurpose or... idk -->
 			<script>
@@ -76,24 +76,43 @@
 	<?php if(FWAPPS_TEMPLATE !== 'login'): ?>
 		cf body-loading
 	<?php else: ?>
-		cf body-loading background-theme-polar
+		cf body-loading background-accent
 	<?php endif; ?>
 ">
 
 <?php if(FWAPPS_TEMPLATE !== 'login' ): ?>
 
 	<div id="timetracker" class="background-theme-contrast">
-		<!-- @if a task is actively selected -->
-			<?php app_get_component('components/sticky-timer'); ?>
+		<!-- @if a task is actively selected and not the timer template -->
+			<?php if(FWAPPS_TEMPLATE !== 'timer' && FWAPPS_TEMPLATE !== 'timer-new'): ?>
+				<?php app_get_component('components/sticky-timer'); ?>
+			<?php endif; ?>
 
 		<main id="timetracker-main" class="flex-xs flex-wrap">
 
 
 			<header id="timetracker-header" class="background-transparent nav color-theme justify-content-space-between theme-inverse position-absolute offset-0-x ">
-				<div class="nav-block background-transparent flex-1-1 justify-content-space-between padding-large-top">
-					<a class="nav-logo nav-block no-padding-x" href="<?=app_create_link(array('template' => 'home')); ?>">
+				<div class="nav-block background-transparent flex-1-1 justify-content-space-between padding-top">
+					<a class="nav-logo nav-block no-padding-x" href="<?=app_create_link(array('template' => 'start')); ?>">
 						<?php app_get_component('components/icon-logo'); ?>
 					</a>
+
+
+					<!-- Start Button -->
+					<?php if(
+						// @deprecated
+							FWAPPS_TEMPLATE == 'home'
+						
+						|| FWAPPS_TEMPLATE == 'start'
+						|| FWAPPS_TEMPLATE == 'timer-sucess'
+
+					): ?>
+						<a href="<?=app_create_link(array('template' => 'start-new')); ?>" class="btn btn-theme-outline padding-x btn-round margin-auto-left margin-small-right">
+							Start New Timer
+						</a>
+					<?php endif; ?>
+
+
 					<a id="timetracker-toggle-view" href="#" class="btn btn-small btn-link btn-symbol color-inherit">
 						<?php app_get_component('components/icon-toggle'); ?>
 					</a>
