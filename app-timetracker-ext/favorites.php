c127ee7d872d6f888817e48b7b50fa8f1a696a20
samzabala <samapitongzabala@gmail.com>
2023-08-25 11:06:55 -0700

    favorite - updated

--- app-timetracker-ext/favorites.php
+++ app-timetracker-ext/favorites.php
@@ -1,4 +1,9 @@
 <div id="timetracker-favorites" class="timetracker-offset-wrapper margin-bottom">
+    <!-- @NOTE div
+        classes to add
+        `background-gradient-45-from-secondary-to-primary` => if timer is not active AND no task is actively selected
+        `background-gradient-tt-active` => if timer is not active AND a task is actively selected
+    -->
     <div class="
         background-gradient-45-from-secondary-to-primary
         radius-large-bottom theme-inverse timetracker-offset-block padding-large-y"></div>
@@ -6,7 +11,7 @@
         <div class="container padding-x timetracker-offset-adjacent-crossover">
             <div class="module">
                 <div class="module-header">
-                    <div class="module-title text-align-center">My Favorites</div>
+                    <div class="h6 no-margin-y text-align-center">My Favorites</div>
                 </div>
                 <div class="module-content no-padding-x no-padding-bottom">
                     <!-- @if has logs -->
