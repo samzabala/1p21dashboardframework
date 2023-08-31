de31067e1e9a5803b82206ee54754d0768478bee
samzabala <samapitongzabala@gmail.com>
2023-08-31 08:30:44 -0700

    login - update

--- app-timetracker-ext/login.php
+++ app-timetracker-ext/login.php
@@ -1,21 +1,51 @@
-<div id="timetracker-login">
+<div id="timetracker-login" class="padding">
 	<main id="timetracker-main" class=" flex-xs flex-wrap justify-content-center align-items-center timetracker-body-login">
-		<div class="module">
-			<div class="module-header">
-				<div class="h1 text-align-center margin-top">Log In</div>
-			</div>
-			<div class="module-content">
-				<div class="flex-grid flex-grid-fixed">
+		<div class="module flex-1-1">
+			<div class="module-content padding-y">
+				<div class="flex-grid flex-grid-fixed flex-grid-compact">
+					<div class="flex-col-xs-12">
+						<div class="input-label font-weight-700 no-padding-y">Log In Using</div>
+					</div>
 					<div class="flex-col-xs-12">
 						<button href="#" class="btn btn-block btn-neutral-outline no-padding-x">
-							<img class="margin-small-right" src="<?=FWAPPS_ROOT_URL ?>/app-<?=FWAPPS_APP?>/assets/images/icon-wrike.svg" alt=""> Login with Wrike
+							<img class="margin-small-right" src="<?=FWAPPS_ROOT_URL ?>/app-<?=FWAPPS_APP?>/assets/images/icon-wrike.svg" alt=""> Wrike
 						</button>
 					</div>
 					<div class="flex-col-xs-12">
 						<button href="#" class="btn btn-block btn-neutral-outline no-padding-x">
-							<img class="margin-right" src="<?=FWAPPS_ROOT_URL ?>/app-<?=FWAPPS_APP?>/assets/images/icon-microsoft.svg" alt=""> Login with Office365
+							<img class="margin-right" src="<?=FWAPPS_ROOT_URL ?>/app-<?=FWAPPS_APP?>/assets/images/icon-microsoft.svg" alt=""> Office365
 						</button>
 					</div>
+				</div>
+				<div class="margin-large-top">
+					<!-- @if user error -->
+						<div class="alert alert-error border-color-error no-radius color-error-dark">
+							Invalid account or password. Please try again.
+						</div>
+
+					<form action="">
+
+						<div class="input-wrapper input-wrapper-block input-wrapper-vertical">
+							<label for="user" class="input-label font-weight-700">Email or Username</label>
+							<input id="user" name="" type="text" placeholder="email@domain.com" class="input input-single-line">
+						</div>
+						<div class="input-wrapper input-wrapper-block input-wrapper-vertical">
+							<label for="password" class="input-label font-weight-700">Password</label>
+							<input id="password" name="" type="password" placeholder="email@domain.com" class="input input-single-line">
+						</div>
+						<div class="p margin-small-top">
+							<a class="color-inherit" href="<?= app_create_link(array('template' => 'login-forgot')); ?> ">Forgot Password</a>
+						</div>
+						<div class="margin-large-top">
+							<button class="btn btn-accent btn-large btn-block">Login</button>
+	
+							<!-- @DEMO only because front end demo will be ugly. use markup after instead -->
+								<a href="<?= app_create_link(array('template' => 'start')) ?>" class="btn btn-accent btn-large btn-block">Login</a>
+								
+							<!-- <button class="btn btn-accent btn-large btn-block">Login</button> -->
+						</div>
+					</form>
+
 				</div>
 			</div>
 		</div>
