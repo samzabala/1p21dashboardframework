a94f2af891db1118a9ff20f84d0ecbf7ac73c71a
samzabala <samapitongzabala@gmail.com>
2023-10-20 14:19:23 -0700

    components

---
+++ app-outreach/components/function-filter-date-range.php
@@ -0,0 +1,21 @@
+<form class="function-select-by-month">
+	<label class="input-label">Date Range:</label>
+	<div class="position-relative display-inline-block">
+		<a href="#" data-toggle-dropdown class="input input-select text-decoration-none">
+			<span class="REPLACE">June 12, 1898 - February 22, 1986</span>
+		</a>
+		<div class="dropdown dropdown-top-flush dropdown-right">
+			<div class="input-wrapper input-wrapper-block input-wrapper-vertical p">
+				<label for="" class="input-label">From</label>
+				<input type="date" class="input input-single-line">
+			</div>
+			<div class="input-wrapper input-wrapper-block input-wrapper-vertical p">
+				<label for="" class="input-label">To</label>
+				<input type="date" class="input input-single-line">
+			</div>
+			<div class="text-align-right">
+				<button class="btn btn-primary">Apply </button>
+			</div>
+		</div>
+	</div>
+</form>
